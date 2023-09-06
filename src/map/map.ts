93a007b1bfd6990d5e0c18d264af36d6f436df6f
WalterBright <ssmbuild@outlook.com>
2023-09-06 22:45:43 +0800

    update: map

---
+++ src/map/map.ts
@@ -0,0 +1,53 @@
+import { GameServer } from '@/common'
+
+export interface MapTheme {
+  order: number
+  nameGLCN: string
+  nameGLEN: string
+  nameALCN: string
+}
+
+export type MapTrackLength = 30 | 60 | 90 | 120
+
+export interface MapTrack {
+  length: MapTrackLength | null
+  mapThemeId: string
+  nameGLCN: string
+  nameGLEN: string
+  nameALCN: string
+
+  // redundant
+  mapThemeNameGLCN: string
+  mapThemeNameALCN: string
+}
+
+export interface CareerSeason {
+  chapter: number
+  order: number
+  server: GameServer
+  name: string
+}
+
+export enum CareerRaceType {
+  Attack = 'attack',
+  Time = 'time',
+  Race = 'race',
+}
+
+export interface CareerRace {
+  order: number
+  type: CareerRaceType
+  limit: string
+  careerSeasonId: string
+  mapTrackId: string
+
+
+  // redundant
+
+  careerSeasonName: string
+  careerSeasonChapter: number
+  careerSeasonServer: GameServer
+  careerSeasonOrder: number
+  mapTrackNameGLCN: string
+  mapTrackNameALCN: string
+}
