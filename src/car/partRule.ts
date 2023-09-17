2b91f55b79f25f6707561f6b231961fe8d819d52
WalterBright <ssmbuild@outlook.com>
2023-09-17 21:56:34 +0800

    0.0.52

---
+++ src/car/partRule.ts
@@ -0,0 +1,129 @@
+import { CarClass } from '@/common'
+
+export interface PartRule {
+  carClass: CarClass
+  star: number
+  uncommonPart: number
+  rarePart: number
+  epicPart: number
+  legendaryPart: number
+}
+
+export const partRuleListGL: PartRule[] = [
+  {
+    carClass: 'D',
+    star: 3,
+    uncommonPart: 5,
+    rarePart: 1,
+    epicPart: 0,
+    legendaryPart: 0,
+  },
+  {
+    carClass: 'D',
+    star: 4,
+    uncommonPart: 7,
+    rarePart: 2,
+    epicPart: 1,
+    legendaryPart: 0,
+  },
+  {
+    carClass: 'D',
+    star: 5,
+    uncommonPart: 9,
+    rarePart: 4,
+    epicPart: 2,
+    legendaryPart: 0,
+  },
+
+  {
+    carClass: 'C',
+    star: 3,
+    uncommonPart: 4,
+    rarePart: 1,
+    epicPart: 1,
+    legendaryPart: 0,
+  },
+  {
+    carClass: 'C',
+    star: 4,
+    uncommonPart: 6,
+    rarePart: 3,
+    epicPart: 1,
+    legendaryPart: 0,
+  },
+  {
+    carClass: 'C',
+    star: 5,
+    uncommonPart: 9,
+    rarePart: 4,
+    epicPart: 2,
+    legendaryPart: 0,
+  },
+
+  {
+    carClass: 'B',
+    star: 3,
+    uncommonPart: 6,
+    rarePart: 1,
+    epicPart: 1,
+    legendaryPart: 0,
+  },
+  {
+    carClass: 'B',
+    star: 4,
+    uncommonPart: 6,
+    rarePart: 4,
+    epicPart: 2,
+    legendaryPart: 0,
+  },
+  {
+    carClass: 'B',
+    star: 5,
+    uncommonPart: 8,
+    rarePart: 5,
+    epicPart: 3,
+    legendaryPart: 0,
+  },
+
+  {
+    carClass: 'A',
+    star: 4,
+    uncommonPart: 5,
+    rarePart: 5,
+    epicPart: 2,
+    legendaryPart: 0,
+  },
+  {
+    carClass: 'A',
+    star: 5,
+    uncommonPart: 6,
+    rarePart: 5,
+    epicPart: 3,
+    legendaryPart: 0,
+  },
+  {
+    carClass: 'A',
+    star: 6,
+    uncommonPart: 6,
+    rarePart: 5,
+    epicPart: 4,
+    legendaryPart: 0,
+  },
+
+  {
+    carClass: 'S',
+    star: 5,
+    uncommonPart: 7,
+    rarePart: 5,
+    epicPart: 3,
+    legendaryPart: 0,
+  },
+  {
+    carClass: 'S',
+    star: 6,
+    uncommonPart: 7,
+    rarePart: 5,
+    epicPart: 4,
+    legendaryPart: 0,
+  },
+]
