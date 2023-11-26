414269afd10640295af76d34eb4e042c69ab43c7
WalterBright <ssmbuild@outlook.com>
2023-11-27 01:11:10 +0800

    update

--- src/car/index.ts
+++ src/car/index.ts
@@ -113,16 +113,19 @@ export interface CarEvent {
   // 活动信息
   getMethod: string
   // 亮相方式不转移到车单，车单专指通行证时代的获取方式。
+  // 更新：亮相方式专门开一个分类总结页面，车单只显示常驻赛事，不稳定的限时赛事自行查询。
   packLevel: number
   storeEpic: boolean
-  storeClash: boolean // 国服擂台赛
+  storeClash: boolean
+  storeGauntlet: boolean // 国服对决擂台赛
   elite: boolean // 国服现在没有了
-  worldLeague: WorldLeague | null // 隐藏车、未发布的车、未加入多人的车
+  worldLeague: WorldLeague | null // 隐藏车、未发布的车、未加入多人的车，赋值为null
   rewardsWorldLeague: WorldLeague[]
   rewardsWorldRank: boolean
   rewardsClassCup: boolean
   rewardsGoal: boolean
   rewardsStarWay: boolean
+  rewardsShowRoom: boolean // 国服还没有上
   rewardsHunt: boolean
 }
 
