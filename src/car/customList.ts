7f4110eaa491ef160ab65d33790b9a2c3085aeac
shensm <shensm@hnao.cn>
2024-08-09 17:05:22 +0800

    0.1.40

--- src/car/customList.ts
+++ src/car/customList.ts
@@ -1,30 +1,19 @@
 import { CarClass, CarQuality, GameServer } from '@/common'
+import type { CarMeta } from '.'
 
 export const customCarListType = [] as const
 
 export type CustomCarListType = (typeof customCarListType)[number]
 
-export interface CustomCar {
-  car_id: string
-  nickName: string
-  fullName: string
-  carClass: CarClass
-  // carClassOrder: number
-  star: number
-  quality: CarQuality
-  isKeyCar: boolean
-  carImageUrl: string
-}
-
 export interface CustomCarList {
   title: string
   desc: string
   server: GameServer
-  mainList: CustomCar[]
+  mainList: CarMeta[]
   subList: {
     title: string
     desc: string
-    main: CustomCar[]
-    sub: CustomCar[]
+    main: CarMeta[]
+    sub: CarMeta[]
   }
 }
